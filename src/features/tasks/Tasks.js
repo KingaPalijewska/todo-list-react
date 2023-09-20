ef0a7d2ada878814b51ab283e116e28fef859296
Kinga Palijewska <kinga.jakubowska09@gmail.com>
2023-09-20 21:27:47 +0200

    added store redux

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {useSelector} from "react-redux";
 import Form from "./Form";
 import TaskList from "./TaskList";
 import Buttons from "./Buttons";
@@ -6,6 +7,8 @@ import Section from "../../common/Section";
 import Header from "../../common/Header";
 import Container from "../../common/Container";
 import { useTasks } from "../../useTasks";
+import {selectTasks} from './tasksSlice';
+
 
 function Tasks() {
   const [hideDone, setHideDone] = useState(false);
@@ -14,8 +17,10 @@ function Tasks() {
     setHideDone(hideDOne => !hideDone);
   };
 
+  const {tasks} = useSelector(selectTasks);
+
   const {
-    tasks,
+    //tasks,
     removeTask,
     toggleTaskDone,
     setAllDone,

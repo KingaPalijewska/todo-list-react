74cae16349afb7506d0bccae9464f1006f3c419d
Kinga Palijewska <kinga.jakubowska09@gmail.com>
2023-10-22 16:08:59 +0200

    update tasksSaga.js

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,16 +1,32 @@
-import { takeEvery, call, put } from "redux-saga/effects";
-import { fetchExampleTasks, setTasks } from "./tasksSlice";
+import { takeEvery, call, put, delay, select, takeEvery, takeLatest } from "redux-saga/effects";
+import {
+    fetchExampleTasks,
+    selectTasks,
+    fetchExampleTasksSuccess,
+    fetchExampleTasksError,
+} from "./tasksSlice";
 import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from ".tasksLocalStorage";
+
 
 function* fetchExampleTasksHandler() {
     try {
+        yield delay(1000);
         const exampleTasks = yield call(getExampleTasks);
-        yield put(setTasks(exampleTasks));
+        yield put(fetchExampleTasksSuccess(exampleTasks));
     } catch (error) {
+        yield put(fetchExampleTasksError());
         yield call(alert, "Coś poszło nie tak!");
     }
 }
-export function* watchFetchExampleTasks() {
-    yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHandler);
 
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage, tasks);
+}
+
+
+export function* tasksSaga() {
+    yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler);
 }

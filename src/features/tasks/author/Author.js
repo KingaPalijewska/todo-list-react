2182618ee4744b9f378b108c4d2f690da1d75898
Kinga Palijewska <kinga.jakubowska09@gmail.com>
2023-10-22 20:21:58 +0200

    added Author.js

---
+++ src/features/tasks/author/Author.js
@@ -0,0 +1,15 @@
+import Container from "../../../common/Container"
+import Section from "../../../common/Section";
+import Header from "../../../common/Header";
+
+export default () => (
+    <Container>
+        <Header title="O autorze" />
+        <Section
+            title="Kinga Palijewska"
+            body={
+                <> nana</>
+            }
+        />
+    </Container>
+);
